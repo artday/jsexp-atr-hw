daf190c7ddd01de6ccdd09d8e19f82fed66ec031
artday <admin@artday.com.ua>
2018-06-10 22:35:07 +0300

    modify film model

--- src/app/film-catalog/film.ts
+++ src/app/film-catalog/film.ts
@@ -1,10 +1,8 @@
 /* Film Model */
 export class Film {
-  constructor(
-    public id: number,
-    public name: string,
-    public year: string,
-    public imgUrl: string,
-    public description: string
-  ) { }
+  id: number;
+  name: string;
+  year: string;
+  imgUrl: string;
+  description: string;
 }
