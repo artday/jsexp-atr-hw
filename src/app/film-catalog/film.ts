f7fd72dd158c7e67cdf0ed535890ebbd4c5a7df7
artday <admin@artday.com.ua>
2018-06-10 22:16:46 +0300

    create Film model

---
+++ src/app/film-catalog/film.ts
@@ -0,0 +1,10 @@
+/* Film Model */
+export class Film {
+  constructor(
+    public id: number,
+    public name: string,
+    public year: string,
+    public imgUrl: string,
+    public description: string
+  ) { }
+}
